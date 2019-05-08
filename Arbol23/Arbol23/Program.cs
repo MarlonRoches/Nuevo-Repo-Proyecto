c9d4598179afb853afbddd3ea97b7653bdb0e267
MarlonRoches <petozza123>
2019-05-07 19:30:43 -0600

    Nuevo

---
+++ Arbol23/Arbol23/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbolB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           ArbolB Arbol = new ArbolB();
+
+
+            string[] lineas = File.ReadAllLines("C:\\Users\\roche\\Desktop\\ProyectoE1\\CSVPrueba.csv");
+
+            foreach (var linea in lineas)
+            {
+                Vehiculo Dato = new Vehiculo
+                {
+                    Id = int.Parse(linea)
+                };
+                Arbol.Insertar(Arbol.Raiz, Dato);
+            }
+
+            bool salir = false;
+            while (salir == false)
+            {
+                Console.WriteLine("Ingrese numero a eliminar");
+                Vehiculo buscar= new Vehiculo();
+                  buscar.Id = int.Parse(Console.ReadLine());
+                Arbol.Eliminar(buscar, Arbol.Raiz);
+                Console.WriteLine("Desea seguir");
+                int seguir = int.Parse(Console.ReadLine());
+                if (seguir == 0)
+                {
+                    salir = true;
+                }
+            }
+            Console.WriteLine("Busqueda");
+            salir = false;
+            while (salir == false)
+            {
+                Console.WriteLine("Ingrese numero a buscar");
+                Vehiculo buscar = new Vehiculo();
+                buscar.Id = int.Parse(Console.ReadLine());
+                Console.WriteLine("Se encontro {0}", Arbol.Busqueda(buscar, Arbol.Raiz));
+                Console.WriteLine("Desea seguir");
+                int seguir = int.Parse(Console.ReadLine());
+                if (seguir == 0) salir = true;
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Arbol23/Arbol23/Vehiculo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbolB
+{
+    public class Vehiculo : IEnumerable
+    {
+        [DisplayName("No. Citas")]
+        public int Id { get; set; }
+        public string Marca { get; set; }
+        public string Linea { get; set; }
+        public DateTime SalidaAlMercado { get; set; }
+        public int CaballosDeFuerza { get; set; }
+        public int CompareTo(object obj)
+        {
+            var comparable = (Vehiculo)obj;
+            return Id.CompareTo(comparable.Id);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
